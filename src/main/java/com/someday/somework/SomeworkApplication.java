c1f042e88b2f3c0a24ec1aaa1c11119d2244bff4
DamirNightCoder89 <keyn_89@mail.ru>
2021-10-12 22:29:29 +0300

    Initial commit

---
+++ src/main/java/com/someday/somework/SomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.someday.somework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/someday/somework/SomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.someday.somework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
